c17e435cbc975ed01eb8c61c07f9fc48723b3fc2
aw <aw@hothpts.com.au>
2026-01-09 09:21:07 +0000

    updates for CORS

--- Program.cs
+++ Program.cs
@@ -5,6 +5,17 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllers();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        builder =>
+        {
+            builder.AllowAnyOrigin()
+                   .AllowAnyHeader()
+                   .AllowAnyMethod();
+        });
+});
+
 builder.Services.AddSingleton<maS.ConsoleLogger>();
 builder.Services.AddSingleton<maS.DatabaseLogger>();
 builder.Services.AddSingleton<maS.ILogger>(sp =>
@@ -22,6 +33,8 @@ var url = $"http://0.0.0.0:{port}";
 var target = Environment.GetEnvironmentVariable("TARGET") ?? "World";
 
 var app = builder.Build();
+
+app.UseCors("AllowAll");
 
 app.MapGet("/", () => $"Hello {target}!");
 
